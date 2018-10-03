0a0f2966ac0f72226dd6a6f0c70e9bd9d472058d
snorrehu <snorre.hukkelaas@gmail.com>
2018-10-03 09:50:08 +0200

    all shit

--- my-app/src/main/java/com/mycompany/app/Application.java
+++ my-app/src/main/java/com/mycompany/app/Application.java
@@ -1,4 +1,6 @@
-package com.mycompany.app;
+package hello;
+
+import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -6,11 +8,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-@SpringApplication
-public class Application
-{
-    public static void main( String[] args )
-    {
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
 }
