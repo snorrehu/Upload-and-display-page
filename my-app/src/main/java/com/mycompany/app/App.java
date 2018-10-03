bcd7ecd4e2c65f1aa1c880d7794795115c6ba5f5
vavvar <haavard.kri@gmail.com>
2018-10-03 11:57:19 +0200

    sad

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,11 +1,27 @@
 package com.mycompany.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.mycompany.app.storage.StorageProperties;
+import com.mycompany.app.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class App {
+	
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
