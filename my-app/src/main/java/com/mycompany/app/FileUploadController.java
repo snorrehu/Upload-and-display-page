6c4c4f5e2fb3c42ea9f70758e0ca876c6615fcdb
vavvar <haavard.kri@gmail.com>
2018-10-04 11:38:21 +0200

    adsaddsa

--- my-app/src/main/java/com/mycompany/app/FileUploadController.java
+++ my-app/src/main/java/com/mycompany/app/FileUploadController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.FileInputStream;
 
+import java.nio.file.*;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -53,7 +54,7 @@ public class FileUploadController {
         return "uploadForm";
     }
 
-    @GetMapping("/files/{filename:.+}")
+    @GetMapping("/{filename:.+}")
     @ResponseBody
     public ResponseEntity<?> serveFile(@PathVariable String filename) {
 
@@ -81,15 +82,40 @@ public class FileUploadController {
         		
     	
   		String fileContent = resultStringBuilder.toString();
-  		System.out.print(String.valueOf(mediaType.get()));
+  		
+  		String header = "<h1>"+filename+"</h1>";
+  		String extension = filename.substring(filename.lastIndexOf("."));
+  		System.out.print(extension);
 
-		if(String.valueOf(mediaType.get()).equals("text/x-java-source")){
-			System.out.print("JAVA FILE!");
-			return ResponseEntity.ok().body("<h1>"+filename+"</h1><br><code>"+fileContent+"</code>");
-		} else {
-			System.out.print("OTHER FILETYPE!");
-			return ResponseEntity.ok().contentType(mediaType.get()).body(file);
+		if(extension.equals(".java") || extension.equals(".cs") || extension.equals(".py") ){
+			System.out.print("CODE FILE!");
+			return ResponseEntity.ok().body(header + "<code>"+fileContent+"</code>");
+		} else if (extension.equals(".txt")){
+			System.out.print("TXT FILE!");
+			return ResponseEntity.ok().body(header + "<p>"+fileContent+"</p>");
+			
+		} else if (extension.equals(".pdf")) {
+
+
+			
+			String filePath = "\""+filename+"\"";
+			//String filePath = "\""+Paths.get(System.getProperty("user.dir")+"/upload-dir/"+filename)+"\"";
+	
+			//return ResponseEntity.ok().contentType(mediaType.get()).body(file);
+			try {return ResponseEntity.ok().body(header + 
+				"<object data="+filePath+" type=\"application/pdf\" width=\"100%\" height=\"800px\"> " +
+				"<iframe src="+filePath+" style=\"border: none;\" width=\"100%\" height=\"800px\"> " +
+				//"This browser does not support PDFs. Please download the PDF to view it: <a href=""\""+file.getFile()+"\">Download PDF</a> " +
+				"</iframe> " + 
+				"</object> "
+		);} 
+			catch(Exception e) {
+
+			}
+		     
 		}
+
+		return ResponseEntity.ok().contentType(mediaType.get()).body(file);
 
     }
 
@@ -112,7 +138,7 @@ public class FileUploadController {
         /*redirectAttributes.addFlashAttribute("message",
                 "You successfully uploaded " + file.getOriginalFilename() + "!");*/
 
-        return "redirect:/files/" + file.getOriginalFilename();
+        return "redirect:/" + file.getOriginalFilename();
     }
 
     @ExceptionHandler(StorageFileNotFoundException.class)
